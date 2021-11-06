bf5473c96cbb1118ca4bdd7a2455e55349ebfa5b
Kirill Maslinsky <kirill@altlinux.org>
2021-11-06 12:45:52 +0300

    remove unnecessary mkdir

--- remote/setup-corpus-environment.sh
+++ remote/setup-corpus-environment.sh
@@ -3,7 +3,7 @@ port="$1"
 shift
 test -d /var/lib/manatee || mkdir /var/lib/manatee
 # setup apache
-mkdir /var/lock/subsys
+mkdir -p /var/lock/subsys
 a2disport http
 cd /etc/httpd2/conf/
 cp ports-available/http-localhost-8088.conf ports-available/"$port".conf
@@ -11,5 +11,3 @@ sed -i "s/8088/$port/g" ports-available/"$port".conf
 sed -i "/^Listen/s/localhost/127.0.0.1/" ports-available/"$port".conf
 a2enport "$port"
 a2enmod cgi
-# required to start httpd service 
-mkdir /var/lock/subsys
