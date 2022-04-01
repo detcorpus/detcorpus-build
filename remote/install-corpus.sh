963e0293d9b72011a8052e7f9fa820ad63ec83ec
Kirill Maslinsky <kirill@altlinux.org>
2022-04-01 12:53:15 +0300

    adapt install-corpus.sh to new syntax

--- remote/install-corpus.sh
+++ remote/install-corpus.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-environment="testing"
+repo="$1"
+shift
+rootdir="$1"
+shift
 corpus="$1"
 shift
-corptar="built/$corpus.tar.xz"
-rm -f "$environment/chroot/.in/${corptar##built/}"
-ln "$corptar" "$environment"/chroot/.in/
-corpnames="$(cat ${corptar%%.tar.xz}.setup.txt | cut -d' ' -f2-)"
+environment="$rootdir/testing"
+corptar="$corpus.tar.xz"
+rm -f "$environment/chroot/.in/$corptar"
+ln "$repo/$corptar" "$environment"/chroot/.in/
+corpnames="$(cat $repo/${corptar%%.tar.xz}.setup.txt | cut -d' ' -f2-)"
 for corpus in $corpnames
 do
     hsh-run --rooter "$environment" -- rm -rf "/var/lib/manatee/{data,registry,vert}/$corpus"
 done
-hsh-run --rooter "$environment" -- tar --no-same-permissions --no-same-owner -xJvf "${corptar##built/}" --directory /var/lib/manatee
+hsh-run --rooter "$environment" -- tar --no-same-permissions --no-same-owner -xJvf "$corptar" --directory /var/lib/manatee
 for corpus in $corpnames
 do
     hsh-run --rooter "$environment" -- /bin/sh -c "export MANATEE_REGISTRY=/var/lib/manatee/registry && mksizes $corpus"
