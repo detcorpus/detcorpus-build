078e55ea690a156297cd0e7e75cbf63c98f95a97
Alena Pestova <alpestova1818@gmail.com>
2022-05-18 16:55:30 +0300

    Добавлена функция для исправления ascii-кавычек в metadata.sql

--- test/check_metadata_sql.py
+++ test/check_metadata_sql.py
@@ -1,6 +1,5 @@
 import argparse
 import re
-import csv
 """
 Скрипт для вывода индексов строк в файле metadata.sql, 
 которые 
@@ -11,8 +10,10 @@ import csv
 
 
 # находит ошибки в файле metadata.sql
-
-def find_errors(file_path):
+def find_errors(file_path, fix=True):
+    # если надо исправлять текст, создаем пустую переменную под исправленный текст
+    if fix is True:
+        new_text = ''
     with open(file_path, 'r') as f:
         # для будущих строк с ошибками
         errors_string = {'text_author': [], 'bad_strings': [], 'ascii': []}
@@ -29,9 +30,15 @@ def find_errors(file_path):
             # найдем место первого INSERT
             if not start_parse:
                 if not line.strip().startswith('INSERT'):
+                    # добавляем строку без изменений в исправленный текст, если это необходимо
+                    if fix is True:
+                        new_text += line
                     continue
                 start_parse = True
             elif line.startswith('COMMIT'):
+                # добавляем строку без изменений в исправленный текст, если это необходимо
+                if fix is True:
+                    new_text += line
                 break
             # строка с INSERT должна полностью соответствовать регулярному выражению
             m = re.match(reg_exp, line)
@@ -53,25 +60,76 @@ def find_errors(file_path):
 
             # проверяем, имеются ли в тексте ascii-кавычки
             values = m.group(2)
+            # для строки с найденными недопустимыми кавычками в значениях
             if values.find('"') >= 0:
                 errors_string['ascii'].append(ind)
+                # если надо исправлять текст
+                if fix is True:
+                    # словарь для сопоставления предыдущей кавычки и подходящей к ней закрывающей (на замену текущей)
+                    quotes_dict = {'«': '»', '„': '“', '“': '”'}
+                    # все допустимые кавычки в значениях
+                    quotes_set = {'«', '»', '„', '“', '“'}
+                    # список текущих кавычек в строке
+                    quotes = []
+                    # создаем новую строку, записываем туда текст до самих значений
+                    new_string = f'INSERT INTO {m.group(1)} VALUES ('
+                    # проходимся посимвольно по строке
+                    for s in values:
+                        # если нашли недопустимую кавычку
+                        if s == '"':
+                            # если сейчас все кавычки закрыты (в строке их парное кол-во)
+                            if len(quotes) % 2 == 0:
+                                # то заменяем " на «
+                                quotes.append('«')
+                                new_string += '«'
+                            # если последние кавычки не закрыты, находим правильную закрывающую кавычку
+                            # на которую меняем "
+                            else:
+                                new_qoute = quotes_dict[quotes[-1]]
+                                new_string += new_qoute
+                                quotes.append(new_qoute)
+                        # если нашли допустимую кавычку, записываем ее
+                        elif s in quotes_set:
+                            quotes.append(s)
+                            new_string += s
+                        # если иной символ - просто присоединяем к новой строке
+                        else:
+                            new_string += s
 
+                    # дописываем в конец новой строки оставшиеся нужные символы
+                    new_string += ');\n'
+                    # добавляем новую строку в исправленный текст
+                    new_text += new_string
+
+            # для строки без недопустимых кавычек, записываем ее в исправленный текст, если это необходимо
+            else:
+                if fix is True:
+                    new_text += line
+
+    if fix is True:
+        return new_text, errors_string
     return errors_string
 
 
 def main(args):
-    errors = find_errors(args.input_file)
+    if args.fix:
+        new_data, errors = find_errors(args.input_file, fix=True)
+        with open(args.input_file, 'w') as f:
+            f.write(new_data)
+    else:
+        errors = find_errors(args.input_file, fix=False)
     # ощибки с переносом строк
     print(f"несоответствие шаблону строки (возможная проблема лишних переносов): {errors['bad_strings']}")
     # повторяющиеся строки в таблице text_author
     print(f"повторяющиеся строки в таблице text_author (повторяющаяся строка, первое вхождение): {errors['text_author']}")
     # ascii символы
-    print(f"ascii-кавычки: {errors['ascii']}")
+    print(f"ascii-кавычки (исправлено): {errors['ascii']}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # путь к файлу metadata.sql
     parser.add_argument("-i", "--input-file", required=True)
+    parser.add_argument("-f", "--fix", required=False, default=True)
     args = parser.parse_args()
     main(args)
