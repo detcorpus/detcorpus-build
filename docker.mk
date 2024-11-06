b4265d82a6f413d900d90529a888352892202d02
Kirill Maslinsky <kmaslinsky@pushdom.ru>
2024-11-06 21:25:46 +0100

    better local docker handling

--- docker.mk
+++ docker.mk
@@ -7,22 +7,31 @@ configfiles := $(patsubst %,config/%,$(corplist))
 corpvertfiles := $(patsubst %,%.vert,$(corplist))
 subcfiles := config/detcorpus-fiction.subcorpora
 archfile := detcorpus.tar.xz 
-exportfiles := $(patsubst %,$(localarch)/registry/%,$(configfiles) $(subcfiles)) $(patsubst %,$(localarch)/vert/%,$(corpvertfiles))
+exportfiles := $(patsubst config/%,$(localarch)/registry/%,$(configfiles) $(subcfiles)) $(patsubst %,$(localarch)/vert/%,$(corpvertfiles))
 exportdirs := $(patsubst %,$(localarch)/%,registry vert)
 packed := $(localarch)/$(archfile)
 
+$(localarch)/registry/% : config/%
+	test -d $(@D) || mkdir -p $(@D)
+	cp -f $< $@
 
-$(exportfiles) : $(configfiles) $(corpvertfiles) 
-	mkdir -p $(exportdirs)
-	cp -f $(configfiles) $(localarch)/registry
-	cp -f $(subcfiles) $(localarch)/registry
-	cp -f $(corpvertfiles) $(localarch)/vert
+$(localarch)/vert/% : %
+	test -d $(@D) || mkdir -p $(@D)
+	cp -f $< $@
 
 pull-image:
 	docker pull maslinych/noske-alt:2.142-alt1
 
-docker-local: $(exportfiles)
-	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p 127.0.0.1:8088:8080 -e CORPLIST="$(corplist)" maslinych/noske-alt:2.142-alt1
+docker-cleanup:
+	test -z "$(shell docker ps -aq -f status=exited -f name=$(corpsite))" || @echo docker rm $(corpsite)
+
+docker-local: $(exportfiles) | docker-cleanup
+	if [ ! "$(shell docker ps -a -q -f name=$(corpsite))" ]; then \
+	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p 127.0.0.1:8088:8080 -e CORPLIST="$(corplist)" maslinych/noske-alt:2.142-alt1 ;\
+	else \
+	@echo "Detcorpus is already running in a local docker container. Stop it first to run anew" ;\
+	fi
+
 
 $(packed) : $(exportfiles)
 	rm -f $@
